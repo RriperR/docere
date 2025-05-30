f56f1dd5b6513c9812bff397485767153fc26532
RriperR <pikuci2006@gmail.com>
2025-05-30 21:14:01 +0300

    add: swagger documentation. upd: frontend design, ci_cd for dev server

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from "react";
-import api from "../api";
-
-function Home() {
-    return <div className="Home">Home Page</div>
-}
-
-
-export default Home;
--- test.py
+++
@@ -1,9 +0,0 @@
-import pydicom
-
-file_path = "30bcii29j69i03011331.dcm"
-
-# Чтение DICOM файла
-dicom_data = pydicom.dcmread(file_path)
-
-# Печать всех метаданных
-print(dicom_data)
